2e391aa30aca6f8b04a0fedb0378d5a379c14f92
adil-shabab <adilshabab2004@gmail.com>
2023-05-23 14:25:51 +0530

    small changes added

--- project/settings.py
+++ project/settings.py
@@ -127,8 +127,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+    
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
